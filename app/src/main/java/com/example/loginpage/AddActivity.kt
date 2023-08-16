54f068d078cd3c0c9a8e3c770d6b7a4a4fa39dc2
Your Name <you@example.com>
2023-08-16 17:02:01 +0300

    committed16.08.17.01

---
+++ app/src/main/java/com/example/loginpage/AddActivity.kt
@@ -0,0 +1,15 @@
+package com.example.loginpage
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class AddActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+
+    }
+}
--- app/src/main/java/com/example/loginpage/NoteAdapter.kt
+++ app/src/main/java/com/example/loginpage/NoteAdapter.kt
@@ -1,19 +1,35 @@
 package com.example.loginpage
 
+import android.app.Instrumentation.ActivityResult
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.TextView
 import android.widget.Toast
+import android.widget.Toolbar
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.loginpage.databinding.ActivityResultBinding
 import com.example.loginpage.databinding.ItemNoteBinding
+import com.google.android.material.appbar.AppBarLayout
 
 class NoteAdapter(private val mContext: Context, private val NoteListesi: List <NoteModel>) :
     RecyclerView.Adapter <NoteAdapter.CardTasarimNesneleriniTutucu >() {
 
-    inner class CardTasarimNesneleriniTutucu (val binding : ItemNoteBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class CardTasarimNesneleriniTutucu (val binding : ActivityResultBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        var menuResim: ImageView=  binding.menuResim
+
+
+
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardTasarimNesneleriniTutucu {
-        val binding = ItemNoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ActivityResultBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CardTasarimNesneleriniTutucu(binding)
     }
 
@@ -29,5 +45,33 @@ class NoteAdapter(private val mContext: Context, private val NoteListesi: List <
         // binding.textView2.text = note.type
 
 
-    }
+
+
+
+        holder.menuResim.setOnClickListener {
+            val popup = PopupMenu(mContext, holder.menuResim)
+            popup.menuInflater.inflate(R.menu.menu, popup.menu)
+            popup.show()
+
+            popup.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.action_add -> {
+                        // "Add" menü öğesine tıklandığında
+                        val intent = Intent(mContext, AddActivity::class.java)
+                        mContext.startActivity(intent)
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+            }
 }
